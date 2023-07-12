c2ad8f6dcaf99c2bfa3eaf4651f139e06d95d165
M04ph3u2 <s.sciacovelli2@studenti.poliba.it>
2023-07-12 21:12:31 +0200

    Implementing routes & constants

--- backend/app.js
+++ backend/app.js
@@ -24,6 +24,6 @@ mongoose.connect('mongodb+srv://admin:9yRFhKEgbzFqQU9i@bestdbever.kplqcma.mongod
 
 // route
 app.use(express.static(path.join(__dirname, "/../frontend/build")));
-app.get('/', (req, res) => {
+app.get('/^\/(?!api).*/', (req, res) => {
     res.sendFile(path.join(__dirname, "/../frontend/build/index.html"));
 });
