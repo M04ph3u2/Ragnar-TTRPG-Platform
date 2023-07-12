f9d8fa728798534a34601fece9d45314ebacd62e
M04ph3u2 <s.sciacovelli2@studenti.poliba.it>
2023-07-12 14:54:27 +0200

    update mongodb uri

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(cors(corsOptions));
 
 // connect MongoDB
-mongoose.connect('mongodb + srv://admin:9yRFhKEgbzFqQU9i@bestdbever.kplqcma.mongodb.net/?retryWrites=true&w=majority').then(() => {
+mongoose.connect('mongodb+srv://admin:9yRFhKEgbzFqQU9i@bestdbever.kplqcma.mongodb.net/?retryWrites=true&w=majority').then(() => {
     const PORT = 4000;
     app.listen(PORT, () => {
         console.log(`App is Listening on PORT ${PORT}`);
